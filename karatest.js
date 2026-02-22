b32a825238667c9ceaba4568a36d0f94504da08f
Tyler R <147563283+Tylerdr16@users.noreply.github.com>
2026-02-22 17:20:07 -0500

    Update karatest.js

--- karatest.js
+++ karatest.js
@@ -3,6 +3,7 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/12.9.0/firebas
 import { getAnalytics } from "https://www.gstatic.com/firebasejs/12.9.0/firebase-analytics.js";
 import { getFirestore, collection, doc, setDoc } from "https://www.gstatic.com/firebasejs/12.9.0/firebase-firestore.js";
 
+//Connects To Firebase
 const firebaseConfig = {
     apiKey: "AIzaSyB5rFKkV-z9XOQ1QnLRDf2sLh1AD7jqj7I",
     authDomain: "karabang-836e4.firebaseapp.com",
@@ -13,12 +14,16 @@ const firebaseConfig = {
     measurementId: "G-TSWBQ6K9GN"
 };
 
+//Creates constants needed to access and send data to firebase.
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
 const db = getFirestore(app);
 
+//Creates const actions to access the Collections of songs and users.
 const songsRef = collection(db, "songs");
+const usersRef = collection(db, "users");
 
+//Function which adds a song and needed info to songs Collection.
 async function addSong() {
     await setDoc(doc(songsRef, "song123"), {
         title: "My Song",
@@ -27,4 +32,19 @@ async function addSong() {
     console.log("Song added!");
 }
 
+//Function which adds a user and needed info to users Collection.
+async function addUser() {
+    await setDoc(doc(usersRef, "92942"), {
+        userID: "01",
+        firstName: "Tyler",
+        lastName: "Radisch",
+        email: "12324KARA@gmail.com",
+        age: "22"
+
+    });
+    console.log("User added!");
+}
+
 addSong();
+addUser();
+
