379591505275bf18a635881f397537f140464809
habibazaraei <habibazaraei24@gmail.com>
2026-05-04 16:10:39 -0400

    pfp

--- src/main/resources/static/js/profilepage.js
+++ src/main/resources/static/js/profilepage.js
@@ -4,6 +4,10 @@ const genreSelect = document.getElementById("genreSelect");
 const genreDisplay = document.getElementById("genreDisplay");
 const saveProfileBtn = document.getElementById("saveProfileBtn");
 
+const uploadPicBtn = document.getElementById("uploadPicBtn");
+const pfpUpload = document.getElementById("pfpUpload");
+const profileImage = document.getElementById("profileImage");
+
 saveProfileBtn.addEventListener("click", function () {
     const newUsername = usernameInput.value.trim();
     const favoriteGenre = genreSelect.value;
@@ -14,6 +18,24 @@ saveProfileBtn.addEventListener("click", function () {
     }
 
     genreDisplay.textContent = favoriteGenre;
+});
+
+uploadPicBtn.addEventListener("click", function () {
+    pfpUpload.click();
+});
+
+pfpUpload.addEventListener("change", function () {
+    const file = this.files[0];
+
+    if (file) {
+        const reader = new FileReader();
+
+        reader.onload = function (event) {
+            profileImage.src = event.target.result;
+        };
+
+        reader.readAsDataURL(file);
+    }
 });
 
 function goToSongs() {
